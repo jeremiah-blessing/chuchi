7fd5209e90b191aa94298122a89b77644a2a7334
Jeremiah Blessing <vjeremiahblessing@gmail.com>
2025-02-16 20:17:28 +0530

    Disable TypeScript ESLint rule and add ignore comments in Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,14 @@ import importMetaUrlPlugin from '@codingame/esbuild-import-meta-url-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
+  // @ts-ignore
   plugins: [react()],
   resolve: {
     dedupe: ['vscode'],
   },
   optimizeDeps: {
     esbuildOptions: {
+      // @ts-ignore
       plugins: [importMetaUrlPlugin],
     },
   },
